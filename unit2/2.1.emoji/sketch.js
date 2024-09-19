8e029d455c848a50c836d65f4159cef8697ceb3f
Kenzie <mswain2@mail.umw.edu>
2024-09-19 19:17:01 -0400

    Rounding rectangle more

--- unit2/2.1.emoji/sketch.js
+++ unit2/2.1.emoji/sketch.js
@@ -9,8 +9,8 @@ function setup() {
 function draw() {
   background(173, 232, 244);
 
-  stroke(0);
+  stroke(100);
   fill(255); 
-  rect(50, 200, 300, 150, 40); 
+  rect(50, 200, 300, 175, 50); 
  
 }
