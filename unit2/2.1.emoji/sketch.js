d664ff4b1f0fd49f2a35557c886c120ef88fa2a4
Kenzie <mswain2@mail.umw.edu>
2024-09-19 19:25:42 -0400

    Adding two white circles

--- unit2/2.1.emoji/sketch.js
+++ unit2/2.1.emoji/sketch.js
@@ -12,5 +12,12 @@ function draw() {
   stroke(100);
   fill(255); 
   rect(50, 200, 300, 175, 50); 
- 
+  
+  stroke(100);
+  fill(255);
+  circle(50, 100, 100);
+
+  stroke(100);
+  fill(255);
+  circle(175, 100, 100);
 }
