2e9cc1fb66699a17624ecf0f1a37623338fce75b
Kenzie <mswain2@mail.umw.edu>
2024-09-24 11:31:55 -0400

    Creating three new files and changing index.html to display Face emoji

---
+++ unit2/2.1.emoji/sketchFace.js
@@ -0,0 +1,17 @@
+//This is the link to the emoji I was trying to recreate. https://emojipedia.org/panda
+//I added some details that aren't on that emoji to make it look nicer. I think it turned out 
+//really well and I really like it!
+
+function setup() {
+  // create a canvas
+  createCanvas(400, 400);
+
+  // disable animation
+  noLoop();
+}
+
+function draw() {
+  //Changing the background color to a light blue
+  background(173, 244, 232);
+
+}
