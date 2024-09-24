b88d7552616d573be9ddb1cf18d918219c1f1d62
Kenzie <mswain2@mail.umw.edu>
2024-09-24 16:43:51 -0400

    Adding circles for bottom of ghost

--- unit2/2.1.altEmojis/sketchghost.js
+++ unit2/2.1.altEmojis/sketchghost.js
@@ -13,8 +13,11 @@ function draw() {
   noStroke();
   fill(240);
   rect(100, 75, 200, 250, 60, 60, 10, 10);
-  circle(125, 325, 50);
-  circle(175, 325, 50);
-  circle(225, 325, 50);
-  
+  circle(125, 320, 50);
+  circle(325, 320, 50);
+  circle(225, 320, 50);
+  fill(150);
+  circle(275, 320, 50);
+  circle(175, 320, 50);
+ 
 }
