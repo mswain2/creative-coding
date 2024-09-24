7a187eeba08b1764473948bd10e27aeda64253c4
Kenzie <mswain2@mail.umw.edu>
2024-09-24 17:08:22 -0400

    Fix tongue and add eyes

--- unit2/2.1.altEmojis/sketchghost.js
+++ unit2/2.1.altEmojis/sketchghost.js
@@ -17,13 +17,20 @@ function draw() {
   circle(200, 320, 40);
   circle(280, 320, 40);
   fill(150);
-  circle(240, 323, 40);
-  circle(160, 323, 40);
+  circle(240, 324, 40);
+  circle(160, 324, 40);
  
   fill("purple");
   angleMode(DEGREES);
   arc(200, 200, 100, 100, 0, 180);
   fill("pink");
-  rect(150, 200, 50, 100, 10, 10, 80, 80);
-    
+  rect(175, 200, 50, 75, 10, 10, 80, 80);
+   
+  fill(0);
+  circle(250, 150, 50);
+  stroke(0);
+  strokeWeight(6);
+  line(125, 125, 150, 150);
+  line(175, 175, 150, 150);
+ 
 }
