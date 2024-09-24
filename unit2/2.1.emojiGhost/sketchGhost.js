3342b4f0ad0f4f03a5b317190b6923b62ca81bb3
Kenzie <mswain2@mail.umw.edu>
2024-09-24 16:02:31 -0400

    Fixing folder organization

---
+++ unit2/2.1.emojiGhost/sketchGhost.js
@@ -0,0 +1,19 @@
+function setup() {
+  // create a canvas
+  createCanvas(400, 400);
+
+  // disable animation
+  noLoop();
+}
+
+function draw() {
+  //Changing the background color to a light grey
+  background(150);
+
+  stroke(100);
+  strokeWeight(6);
+  fill(240);
+  rect(150, 200, 100, 250, 60, 60, 10, 10);
+
+  
+}
