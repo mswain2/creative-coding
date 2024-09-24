381bc15c6a9ada660a6227366097544461ac4da0
Kenzie <mswain2@mail.umw.edu>
2024-09-24 17:17:43 -0400

    Fixing winky eye and adding comments

--- unit2/2.1.altEmojis/sketchghost.js
+++ unit2/2.1.altEmojis/sketchghost.js
@@ -1,3 +1,6 @@
+//Here is the link for the emoji I recreated. https://emojipedia.org/ghost
+//I wanted to create another character that wasn't an animal and wasn't a face.
+
 function setup() {
   // create a canvas
   createCanvas(400, 400);
@@ -9,28 +12,37 @@ function setup() {
 function draw() {
   //Changing the background color to a light grey
   background(150);
-
+   
+  //Change to no stroke and make the color white. Create a rounded rectangle for the top of the ghost.
   noStroke();
   fill(240);
   rect(100, 75, 200, 250, 80, 80, 10, 10);
+  //Create white circles to represent the bottom of the ghost.
   circle(120, 320, 40);
   circle(200, 320, 40);
   circle(280, 320, 40);
+  //Create some circles that are the same color as the background to make folds in the bottom of the ghost.
   fill(150);
   circle(240, 324, 40);
   circle(160, 324, 40);
  
+  //Make the fill purple and make a half circle to represent the mouth.
   fill("purple");
   angleMode(DEGREES);
   arc(200, 200, 100, 100, 0, 180);
+  
+  //Make the fill pink and a half rounded rectangle to represent the tongue.
   fill("pink");
   rect(175, 200, 50, 75, 10, 10, 80, 80);
    
+  //Make the fill to zero and create a circle for the open eye.
   fill(0);
   circle(250, 150, 50);
+
+  //Make the stroke black and create two lines to make the winky eye.
   stroke(0);
   strokeWeight(6);
-  line(130, 130, 150, 150);
-  line(130, 170, 150, 150);
+  line(130, 130, 175, 150);
+  line(130, 170, 175, 150);
  
 }
