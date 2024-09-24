62b5282ff72134d8d636345a239d4068cc9860ae
Kenzie <mswain2@mail.umw.edu>
2024-09-24 11:37:06 -0400

    Added yellow circle for head and eyes

--- unit2/2.1.emoji/sketchFace.js
+++ unit2/2.1.emoji/sketchFace.js
@@ -12,6 +12,19 @@ function setup() {
 
 function draw() {
   //Changing the background color to a light blue
-  background(173, 244, 232);
+  background(173, 244, 222);
 
+  stroke("gold"); // set the stroke (line) color 
+  strokeWeight(6); // set the stroke thickness
+  fill("yellow"); // set the fill color
+
+  // draw an ellipse with 250 pixel diameter
+  // with its center at x position 200
+  // and at y position 200
+  ellipse(200,200,250,250) 
+
+  noStroke();
+  fill(255);
+  circle(150, 175, 75);
+  circle(250, 175, 75);
 }
