ddcd5568f2a4fd1fd1b0e98eb99a59fb03b98fe2
Kenzie <mswain2@mail.umw.edu>
2024-09-24 15:53:43 -0400

    Fixing zipper thing

--- unit2/2.1.emoji/sketchFace.js
+++ unit2/2.1.emoji/sketchFace.js
@@ -1,3 +1,6 @@
+//Here is the link to the emoji I recreated. https://emojipedia.org/zipper-mouth-face
+//I wanted to do a face, but didn't want to choose an easy/plain one.
+
 function setup() {
   // create a canvas
   createCanvas(400, 400);
@@ -7,7 +10,7 @@ function setup() {
 }
 
 function draw() {
-  //Changing the background color to a light blue
+  //Changing the background color to a light green
   background(173, 244, 222);
 
   stroke("gold"); // set the stroke (line) color 
@@ -19,18 +22,23 @@ function draw() {
   // and at y position 200
   ellipse(200,200,250,250); 
 
+  //Change to noStroke and make the fill white. Create two circles for the eyes.
   noStroke();
   fill(255);
   circle(150, 175, 75);
   circle(250, 175, 75);
 
+  //Change the fill to black and make two circles for the pupils.
   fill(0);
   circle(150, 175, 30);
   circle(250, 175, 30);
 
+  //Change the fill to a gray color and create the long rectanlge for the length of the zipper.
   fill(150);
   rect(114, 240, 175, 17);
 
+  //Create different ellipses to represent the teeth of the zipper. They are offset
+  //By a certain amount and flip over the rectangle created as the middle line.
   ellipse(124, 253, 20, 25);
   ellipse(144, 243, 20, 25);
   ellipse(164, 253, 20, 25);
@@ -40,13 +48,15 @@ function draw() {
   ellipse(244, 253, 20, 25);
   ellipse(264, 243, 20, 25);
 
+  //Create and rotate the actual zipper thing that you pull. 
   push();
   angleMode(DEGREES);
-  translate(283, 245);
+  translate(283, 250);
   rotate(-45);
   rect(0, 0, 25, 35, 50, 50, 20, 20);  
   pop(); 
  
+  //Create a small yellow circle to place over the zipper pull thing to make it look like a hole.
   fill("yellow");
-  circle(285, 250, 10);
+  circle(295, 250, 10);
 }
