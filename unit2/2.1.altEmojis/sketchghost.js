b31743d7d92830b2231e94ae81ad8291f94c30ad
Kenzie <mswain2@mail.umw.edu>
2024-09-24 17:30:32 -0400

    Fixing square

--- unit2/2.1.altEmojis/sketchghost.js
+++ unit2/2.1.altEmojis/sketchghost.js
@@ -24,8 +24,8 @@ function draw() {
   //Create two lines to represent the arms.
   stroke(240);
   strokeWeight(50);
-  line(110, 225, 70, 180);
-  line(290, 225, 330, 180);
+  line(110, 225, 80, 180);
+  line(290, 225, 320, 180);
 
   //Create some circles that are the same color as the background to make folds in the bottom of the ghost.
   noStroke();
