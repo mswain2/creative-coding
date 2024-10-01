ecf70653bc67abce3e44b03fe056474f4a54a707
Kenzie <mswain2@mail.umw.edu>
2024-10-01 15:27:30 -0400

    Completed iteration of the grid exercise

--- unit2/2.2.grid/sketch.js
+++ unit2/2.2.grid/sketch.js
@@ -9,43 +9,71 @@ function setup() {
 function draw() {
   background(220);
 
- //Translate off of the edge of the canvas.
- translate(70, 70);
+  //Create variables so the number of rows and columns, the width, and the height of each rectangle can easily
+  //be changed.
+  let numRects = 6;
+  let width = 90;
+  let height = 70;
   
-  //Use a for loop to draw 8 columns.
-  for(let x = 0; x < 8; x++){
-    //Use a for loop to draw 8 rows.
-    for(let y = 0; y < 8; y++){
+  //Calculate the distance between each rectange. Total canvas - space taken up by actual rectangles
+  //divided by the total number of spaces needed (one more than the number of rectangles to account for the border.
+  let dist = (1200 - (numRects * width)) / (numRects + 1);
+  
+  //Calculate how much translate needs to move to accommodate the distance between rectangles and the width
+  //of the rectangles.
+  let move = dist + width;
+
+ //Translate off of the edge of the canvas.
+ translate(dist + width/2, dist + width/2);
+  
+  //Use a for loop to draw specified number of columns.
+  for(let x = 0; x < numRects; x++){
+    //Use a for loop to draw specified number of rows.
+    for(let y = 0; y < numRects; y++){
         //Push and pop to only temporarily rotate and transform.
         push();
 
+        //Create a list of three colors to restrict the amount of randomness in colors.
+        let colors = ['Aquamarine', 'DarkOrchid', 'HotPink'];
+
+        //Randomly select a color for the fill
+        let fillVal = random(colors);
+
+        //Randomly select a color for the stroke.
+        let strokeVal = random(colors);
+
         //Generate 3 random integer values to serve as the rgb values for the fill.
-        let r = random(255);
-        let g = random(255);
-        let b = random(255);
+        //let r = random(255);
+        //let g = random(255);
+        //let b = random(255);
+        
         //Use the randomized values to set the fill to a random color.
-        fill(r, g, b);
+        fill(fillVal);
 
         //Generate 3 more random integer values to serve as the rgb values for the stroke.
-        let sr = random(255);
-        let sg = random(255);
-        let sb = random(255);
+        //let sr = random(255);
+        //let sg = random(255);
+        //let sb = random(255);
+        
         //Use the above randomized values to set the stroke to a random color.
-        stroke(sr, sg, sb);
+        stroke(strokeVal);
         //Randomize the stroke weight. I randomly decided to use only up to a stroke weight of 10.
         strokeWeight(random(10));
 
         //Translate to the next spot to draw a rectangle.
-        translate(x * 150, y * 150);
+        translate(x * move, y * move);
+        
         //Change the angle mode and the rectMode.
         angleMode(DEGREES);
         rectMode(CENTER);
+        
         //Randomly generate a number to rotate the rectangle. I wanted to add movement instead of just
         //changing the colors and sizes.
-        rotate(random(350));
+        //Restrict the angles that the rectangles can rotate.
+        rotate(random(-30, 30));
 
         //Draw the rectangle using randomly generated width and height.
-        rect(0, 0, random(140), random(140));
+        rect(0, 0, width, height);
         pop();
     }
   }
