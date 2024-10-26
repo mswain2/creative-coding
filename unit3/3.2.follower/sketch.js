789916b690ae703718fa4bff35144073952da36b
Kenzie <mswain2@mail.umw.edu>
2024-10-26 16:36:50 -0400

    Added moving and following logic

--- unit3/3.2.follower/sketch.js
+++ unit3/3.2.follower/sketch.js
@@ -8,10 +8,38 @@ function setup() {
   createCanvas(800, 800);
 }
 
+
+let speedfactor = 3;
+let xspeed = speedfactor;
+let yspeed = speedfactor;
+let x = 50;
+let y = 50;
+
+
 function draw() {
   background("MediumSeaGreen");
 
-  image(img, 10, 10, 50, 50);
-   
+  image(img, x-25, y-25, 50, 50);
+ 
+  if (mouseX > x){
+    xspeed = speedfactor;
+  }else{
+    xspeed = -speedfactor;
+  }
+
+  if (mouseY > y){
+    yspeed = speedfactor;
+  }else{
+    yspeed = -speedfactor;
+  }
+ 
+  let d = sqrt((x - mouseX)**2 + (y - mouseY)**2);  
+
+  if(d <= 5){
+    x = 50;
+    y = 50;
+    xspeed = speedfactor;
+    yspeed = speedfactor; 
+  }
 
 }
