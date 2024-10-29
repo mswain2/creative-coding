36e5d1e0975e96a49a94862bcf3a61b9fb000ce1
Kenzie <mswain2@mail.umw.edu>
2024-10-29 12:08:52 -0400

    Completed ripples variation

--- unit3/3.3.ripples/sketch.js
+++ unit3/3.3.ripples/sketch.js
@@ -3,11 +3,13 @@
 //and only did a few circles since real ripples are centered around a main circle. I think
 //this turned out really well and I like how realistic it looks.
 
+//I chose to do the variation of making the ripples reflect off of the edge of the canvas. I also
+//made it so the ripples would disappear.
+
 function setup() {
   createCanvas(800, 800);
-  //Set to no fill and change the stroke color to a light blue.
+  //Set to no fill.
   noFill();
-  stroke("LightBlue");
 }
 
 //Create an empty array that will hold the ripples.
@@ -16,15 +18,19 @@ let ripplers = [];
 //Create a class for the ripplers.
 class Rippler{
     //Constructor to make the ripple objects.
-    constructor(x, y){
+    constructor(x, y, diam){
         //Set x, y, and the diameter.
         this.x = x;
         this.y = y;
-        this.diameter = 0;
+        this.diameter = diam;
+        //Set the alpha value to fully opaque
+        this.alpha = 255;
     }
 
     //Draw method
     draw(){
+        //Set the color to a light blue and set the alpha value to the personal value.
+        stroke(173, 216, 230, this.alpha);
         //Increment the diameter.
         this.diameter += 2;
         //Draw the outermost ripple.
@@ -37,6 +43,33 @@ class Rippler{
         if (this.diameter > 160){
             circle(this.x, this.y, this.diameter-140);
         }
+        //If the left side of the ripple is near the left wall, create a new ripple as a reflection.
+        if(isNear(this.x-this.diameter/2, 0, 0.7) && this.y > 0 && this.y <= 800){
+            let r = new Rippler(this.x - this.diameter/2, this.y, 40);
+            ripplers.push(r); 
+        }
+        //If the right side of the ripple is near the right wall, create a new ripple as a reflection
+        if(isNear(this.x+this.diameter/2, 800, 0.7) && this.y > 0 && this.y <= 800){
+            let r = new Rippler(this.x + this.diameter/2, this.y, 40);
+            ripplers.push(r); 
+        }
+        //If the top of the ripple is near the top wall, create a new ripple as a reflection
+        if(isNear(this.y-this.diameter/2, 0, 0.7) && this.x > 0 && this.x <= 800){
+            let r = new Rippler(this.x, this.y-this.diameter/2, 40);
+            ripplers.push(r);
+        }
+        //If the bottom of the ripple is near the bottom wall, create a new ripple as a reflection
+        if(isNear(this.y+this.diameter/2, 800, 0.7) && this.x > 0 && this.x <= 800){
+            let r = new Rippler(this.x, this.y+this.diameter/2, 40);
+            ripplers.push(r);
+        }
+        //If the alpha value is low enough to not see the ripple, remove it from the list of ripples.
+        if(this.alpha == 20){
+            let index = ripplers.indexOf(this);
+            ripplers.splice(index, 1);
+        }
+        //Decrement the alpha value.
+        this.alpha = this.alpha - 0.5;
     }
 }
 
@@ -54,7 +87,26 @@ function draw() {
 //Mouse pressed function
 function mousePressed(){
     //Create a new rippler with the x and y values equal to where the cursor clicked.
-    let r = new Rippler(mouseX, mouseY);
+    let r = new Rippler(mouseX, mouseY, 0);
     //Push the new rippler to the array.
     ripplers.push(r);
 }
+
+
+//function to determine whether a number is near a certain target
+function isNear(num, target, threshold){
+    //If the number is past the bounds, return false.
+    if(num < 0 || num > 800.2){
+        return false;
+    }
+   
+    //Find the difference between the given number and the target. 
+    let diff = Math.abs(num - target);
+    //If the difference is less than the given threshold, return true.
+    if(diff < threshold){
+        return true;
+    }else{
+        return false;
+    }
+}
+
--- unit3/3.x.Eyeballs/sketch.js
+++ unit3/3.x.Eyeballs/sketch.js
@@ -1,7 +1,58 @@
 function setup() {
   createCanvas(800, 600);
+  angleMode(DEGREES);
 }
+
+let x;
+let y;
+let ang = 0;
 
 function draw() {
   background(0);
+ 
+  fill("Indigo");
+  circle(400, 300, 200);
+  
+  fill("white");
+  
+ /* // left eye
+  lx = 150;
+  ly = 170;
+  
+  // distance (hypotenuse) between mouse and point
+  let ld = sqrt((mouseX - lx)**2 + (mouseY - ly)**2)
+  
+  if (mouseX > lx){
+    lang = asin((mouseY - ly) / ld);
+  }else{
+    lang = (90 - asin((mouseY - ly) / ld)) + 90;
+  }
+  push();
+  translate(lx,ly);
+  rotate(lang);
+  ellipse(0,0,70,70);
+  fill(0);
+   circle(20,0,30);
+  pop();
+  */
+  // right eye
+  x = 400;
+  y = 300;
+  
+  // distance (hypotenuse) between mouse and point
+  let rd = sqrt((mouseX - x)**2 + (mouseY - y)**2)
+  
+  if (mouseX > x){
+    rang = asin((mouseY - y) / rd);
+  }else{
+    rang = (90 - asin((mouseY - y) / rd)) + 90;
+  }
+  push();
+  translate(x,y);
+  rotate(rang);
+  ellipse(0,0,80,80);
+  fill(0);
+  circle(20,0,40);
+  pop();
 }
+
